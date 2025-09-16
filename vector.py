ae2642c958625d1741cbdfeb4a1110cdf40c5e57
TharaBhaiDoraemon <shantil.khadatkar@gmail.com>
2025-09-17 02:18:32 +0530

    Initial commit

---
+++ vector.py
@@ -0,0 +1,38 @@
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import PyPDFLoader, CSVLoader, UnstructuredWordDocumentLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+
+embeddings = OllamaEmbeddings(model="bge-m3")
+db_location = "./chrome_langchain_db"
+vector_store = Chroma(
+    collection_name="restaurant_reviews",
+    persist_directory=db_location,
+    embedding_function=embeddings
+)
+
+if not os.path.exists(db_location) or vector_store._collection.count() == 0:
+    attachments_dir = "attachments"
+    all_documents = []
+    for filename in os.listdir(attachments_dir):
+        file_path = os.path.join(attachments_dir, filename)
+        if filename.endswith(".pdf"):
+            loader = PyPDFLoader(file_path)
+            documents = loader.load_and_split()
+        elif filename.endswith(".csv"):
+            loader = CSVLoader(file_path)
+            documents = loader.load()
+        elif filename.endswith(".docx"):
+            loader = UnstructuredWordDocumentLoader(file_path)
+            documents = loader.load_and_split()
+        else:
+            continue
+        all_documents.extend(documents)
+    
+    if all_documents:
+        vector_store.add_documents(documents=all_documents)
+
+retriever = vector_store.as_retriever(
+    search_kwargs={"k": 5}
+)
